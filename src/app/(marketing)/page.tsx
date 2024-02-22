e0675a6244231e7989720eee9d1cb3adb6cfe879
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-22 08:53:35 -0400

    hyperlinked button elements on landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen bg-main flex flex-col lg:flex-row items-center justify-center gap-x-10">
@@ -22,8 +23,12 @@ export default function Home() {
           access for $299.
         </p>
         <div className="mt-10 space-x-4">
-          <Button>Get Started</Button>
-          <Button variant="secondary">Log in</Button>
+          <Button asChild>
+            <Link href="/sign-up">Get Started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/sign-in">Log in</Link>
+          </Button>
         </div>
       </div>
     </main>
