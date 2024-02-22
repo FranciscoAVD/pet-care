a3673d7585d197904015e4f9243a1ba9594bfa74
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-22 13:13:12 -0400

    changed parameter type of setPets in pet store

--- src/stores/pet-store.ts
+++ src/stores/pet-store.ts
@@ -5,13 +5,13 @@ type TPetStore = {
     activePet: string;
     pets: TPet[];
     setActivePet: (id: string) => void;
-    setPets: (pet: TPet) => void;
+    setPets: (pets: TPet[]) => void;
 }
 export const usePetStore = create<TPetStore>((set,get) =>({
     activePet: '-1',
     pets: [],
     setActivePet: (id) => set(()=>({activePet: id})),
-    setPets: (pet) => {
-        set(state => ({pets: [...state.pets, pet]}))
+    setPets: (pets) => {
+        set(() => ({pets: [...pets]}))
     }
 }))
