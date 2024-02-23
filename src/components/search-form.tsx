32bb708f282a449b1a3081c5daae8370306f4473
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-23 13:39:47 -0400

    added state to track user input

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,8 +1,15 @@
+"use client"
+import { useSearchStore } from "@/stores/search-store";
+import { useState } from "react";
 export default function SearchForm() {
+  const search = useSearchStore(state=> state.search)
+  const set = useSearchStore(state=> state.setSearch)
   return (
     <form action="" className="w-full h-full">
       <input
-        type="text"
+        type="search"
+        value={search}
+        onChange={e=>set(e.target.value)}
         className="w-full h-full bg-white rounded-md px-4 focus:outline-main"
         placeholder="Search for a guest"
       />
