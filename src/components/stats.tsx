5688eaeb8f50091e0d144920df160d5f1d118bb6
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-23 13:39:04 -0400

    gave component props to accept length of pet array

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,7 +1,7 @@
-export default function Stats() {
+export default function Stats({guests}:{guests:number}) {
   return (
     <section>
-      <p className="text-2xl font-bold leading-6 text-white text-center">2</p>
+      <p className="text-2xl font-bold leading-6 text-white text-center">{guests}</p>
       <p className="opacity-50">current guests</p>
     </section>
   );
