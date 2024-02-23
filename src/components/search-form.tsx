5b9831ea19b3aa07897462edf9e28eef5e6b4200
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-23 09:09:48 -0400

    changed the style of the input in form component and added a placeholder

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,7 +1,11 @@
 export default function SearchForm() {
   return (
     <form action="" className="w-full h-full">
-      <input type="text" className="w-full h-full bg-white/20 rounded-md"/>
+      <input
+        type="text"
+        className="w-full h-full bg-white rounded-md px-4 focus:outline-main"
+        placeholder="Search for a guest"
+      />
     </form>
   );
 }
