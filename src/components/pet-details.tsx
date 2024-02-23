d60af150efe9f0128bec739b23f40692d9b8d5bc
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-02-23 10:59:54 -0400

    finished styling component. Added prop to the component to receive the pets array from api call

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -1,20 +1,93 @@
-import Image from "next/image";
+"use client";
 
-export default function PetDetails() {
+import { TPet } from "@/lib/types";
+import Image from "next/image";
+import { usePetStore } from "@/stores/pet-store";
+import { Button } from "./ui/button";
+import { placeholderUrl } from "@/lib/constants";
+
+export default function PetDetails({ pets }: { pets: TPet[] }) {
+  const getPet = usePetStore((state) => state.getActivePet);
+  const id = usePetStore((state) => state.activePet);
+  const pet = getPet(pets, id);
   return (
-    <section className="h-full w-full text-white">
-      <div className="flex items-center">
+    <section className="flex flex-col h-full w-full">
+      {pet ? (
+        <>
+          <TopBar url={pet.imageUrl} name={pet.name} />
+          <OtherInfo owner={pet.ownerName} age={pet.age} />
+          <Notes notes={pet.notes} />
+        </>
+      ) : (
+        <NoPet />
+      )}
+    </section>
+  );
+}
+
+function NoPet() {
+  return (
+    <section className="flex items-center px-8 py-5 bg-white border-b border-black/10">
         <Image
-          src=""
+          src={placeholderUrl}
           alt="Pet Image"
           width={75}
           height={75}
           className="w-[75px] h-[75px] rounded-full object-cover"
         />
         <h2 className="text-3xl font-semibold leading-7 ml-5">
-            Pet Name
-        </h2>
+          No Pet Selected
+        </h2>      
+    </section>
+  );
+}
+
+function TopBar({ url, name }: { url: string; name: string }) {
+  return (
+    <section className="flex items-center justify-between px-8 py-5 bg-white border-b border-black/10">
+      <div className="flex items-center">
+        <Image
+          src={url}
+          alt="Pet Image"
+          width={75}
+          height={75}
+          className="w-[75px] h-[75px] rounded-full object-cover"
+        />
+        <h2 className="text-3xl font-semibold leading-7 ml-5">{name}</h2>
       </div>
+      <div className="space-x-2">
+        <Button>Edit</Button>
+        <Button variant="secondary" className="hover:text-accent">
+          Checkout
+        </Button>
+      </div>
+    </section>
+  );
+}
+
+function OtherInfo({ owner, age }: { owner: string; age: number }) {
+  return (
+    <section className="flex justify-between px-8 py-10 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-gray-700 ">
+          owner name
+        </h3>
+        <p className="mt-1 text-lg text-gray-700 ">{owner}</p>
+      </div>
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-gray-700 ">
+          age
+        </h3>
+        <p className="mt-1 text-lg text-gray-700 ">{age}</p>
+      </div>
+    </section>
+  );
+}
+
+function Notes({ notes }: { notes: string }) {
+  return (
+    <section className="grow bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-black/10">
+      {notes}
     </section>
   );
 }
