4b847e0a3cbc080217cc265326b500a2ad502f57
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-04 14:25:50 -0400

    file for storing new users and patching user info

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { mutation } from "./_generated/server";
-
+import { useConvexAuth } from "convex/react";
 export const store = mutation({
     args: {},
     handler: async (ctx) => {
@@ -7,7 +7,7 @@ export const store = mutation({
       if (!identity) {
         throw new Error("Called storeUser without authentication present");
       }
-  
+      
       // Check if we've already stored this identity before.
       const user = await ctx.db
         .query("users")
@@ -17,14 +17,14 @@ export const store = mutation({
         .unique();
       if (user !== null) {
         // If we've seen this identity before but the name has changed, patch the value.
-        if (user.name !== identity.name) {
-          await ctx.db.patch(user._id, { name: identity.name });
+        if (user.userName !== identity.nickname) {
+          await ctx.db.patch(user._id, { userName: identity.nickname });
         }
         return user._id;
       }
       // If it's a new identity, create a new `User`.
       return await ctx.db.insert("users", {
-        name: identity.name!,
+        userName: identity.nickname!,
         tokenIdentifier: identity.tokenIdentifier,
       });
     },
