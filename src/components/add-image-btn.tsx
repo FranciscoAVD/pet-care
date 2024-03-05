b20448e6a1b7b8a319396260af339eb3d187349b
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-05 17:13:58 -0400

    component now successfully uploads docs to db

--- src/components/add-image-btn.tsx
+++ src/components/add-image-btn.tsx
@@ -2,7 +2,21 @@
 import { UploadButton, UploadFileResponse } from "@xixixao/uploadstuff/react";
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
-export default function ImageButton(){
-    const generateUploadUrl = useMutation(api.files.generateUploadUrl);
-    return <UploadButton uploadUrl={""}/>
+import { useState } from "react";
+export default function ImageButton({  }) {
+  const [uploadId, setUploadId] = useState<null | string>(null);
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const saveAfterUpload = (uploaded: UploadFileResponse[]) => {
+    setUploadId((uploaded[0].response as any).storageId);
+  };
+  return (
+    <>
+      <UploadButton
+        uploadUrl={generateUploadUrl}
+        fileTypes={["image/*"]}
+        onUploadComplete={saveAfterUpload}
+        onUploadError={(error: unknown) => alert(`ERROR: ${error}`)}
+      />
+    </>
+  );
 }
