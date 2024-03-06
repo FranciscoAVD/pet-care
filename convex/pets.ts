6d79c1b839d1ecfe97e121a577c2922c93a24402
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-06 08:30:25 -0400

    file for CRUD operations on pets table

--- convex/pets.ts
+++ convex/pets.ts
@@ -1,25 +1,37 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const addPet = mutation({
-    // You can customize these as you like
-    args: {
-      storageId: v.optional(v.id("_storage")),
-      name: v.string(),
-      owner: v.string(),
-      age: v.number(),
-      notes: v.optional(v.string()),
-      user: v.id("users")
-    },
-    handler: async (ctx, args) => {
-      const res = await ctx.db.insert("pets", {
-        imageStorageId: args.storageId,
-        owner: args.owner,
-        age: args.age,
-        notes: args.notes,
-        name: args.name,
-        careTaker: args.user
-      });
-      return res
-    },
-  });
+  // You can customize these as you like
+  args: {
+    storageId: v.optional(v.id("_storage")),
+    name: v.string(),
+    owner: v.string(),
+    age: v.number(),
+    notes: v.optional(v.string()),
+    user: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const res = await ctx.db.insert("pets", {
+      imageStorageId: args.storageId,
+      owner: args.owner,
+      age: args.age,
+      notes: args.notes,
+      name: args.name,
+      careTaker: args.user,
+    });
+    return res;
+  },
+});
+export const getPets = query({
+  args: {
+    user: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const res = await ctx.db
+      .query("pets")
+      .withIndex("by_careTaker", (q) => q.eq("careTaker", args.user))
+      .collect();
+    return res;
+  },
+});
