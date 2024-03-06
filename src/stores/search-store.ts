48724baf54716f5b90022ec51eeedf808d9c8510
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-06 16:14:51 -0400

    filtersearch now accepts a possibly undefined value to be able to process pets from convex query

--- src/stores/search-store.ts
+++ src/stores/search-store.ts
@@ -4,17 +4,20 @@ import { create } from "zustand";
 type TSearchStore = {
   search: string;
   setSearch: (search: string) => void;
-  filterSearch: (p: TPet[], s: string) => TPet[];
+  filterSearch: (p: TPet[] | undefined, s: string) => TPet[];
 };
 export const useSearchStore = create<TSearchStore>((set, get) => ({
   search: "",
   setSearch: (s) => {
     set(() => ({ search: s }));
   },
-  filterSearch: (pets: TPet[], s) => {
-    const filtered = pets.filter((p) =>
-      p.name.toLocaleLowerCase().includes(s.toLowerCase())
-    );
-    return filtered;
+  filterSearch: (pets, s) => {
+    if (pets) {
+      const filtered = pets.filter((p) =>
+        p.name.toLocaleLowerCase().includes(s.toLowerCase())
+      );
+      return filtered;
+    }
+    return [];
   },
 }));
