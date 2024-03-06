c5150d16afd537744810507a830cbfaab58bb9f4
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-06 16:23:02 -0400

    receives guests property from derived state from the parent page.tsx

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,7 +1,8 @@
-export default function Stats({guests}:{guests:number}) {
+export default function Stats({guests}:{guests:number|undefined}) {
+  
   return (
     <section>
-      <p className="text-2xl font-bold leading-6 text-white text-center">{guests}</p>
+      <p className="text-2xl font-bold leading-6 text-white text-center">{guests?guests:0}</p>
       <p className="opacity-50">current guests</p>
     </section>
   );
