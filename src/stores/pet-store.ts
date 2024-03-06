346a50be097f6a61423b3d2979e7d46eacb3f594
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-06 16:16:42 -0400

    getactive pet now accepts possibly undefined pets array

--- src/stores/pet-store.ts
+++ src/stores/pet-store.ts
@@ -2,19 +2,19 @@ import { TPet } from "@/lib/types";
 import { create } from "zustand";
 
 type TPetStore = {
-    activePet: string;
-    pets: TPet[];
-    setActivePetId: (id: string) => void;
-    getActivePet: (pets: TPet[], id: string) => TPet|null;
-}
-export const usePetStore = create<TPetStore>((set,get) =>({
-    activePet: '-1',
-    pets: [],
-    setActivePetId: (id) => {
-        set(()=>({activePet: id}))
-    },
-    getActivePet: (pets: TPet[], id) => {
-        const active = pets.find(p => p.id === id);
-        return active ? active : null;
-    }
-}))
+  activePet: string;
+  pets: TPet[];
+  setActivePetId: (id: string) => void;
+  getActivePet: (pets: TPet[] | undefined, id: string) => TPet | null;
+};
+export const usePetStore = create<TPetStore>((set) => ({
+  activePet: "-1",
+  pets: [],
+  setActivePetId: (id) => {
+    set(() => ({ activePet: id }));
+  },
+  getActivePet: (pets, id) => {
+    const active = pets?.find((p) => p._id === id);
+    return active ? active : null;
+  },
+}));
