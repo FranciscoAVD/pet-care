758058d74e4bd66fb74cb401b26f50afcad8c69b
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-07 16:09:24 -0400

    added conditional values for filterSearch based on pets query from db. comments inside function

--- src/stores/search-store.ts
+++ src/stores/search-store.ts
@@ -4,7 +4,7 @@ import { create } from "zustand";
 type TSearchStore = {
   search: string;
   setSearch: (search: string) => void;
-  filterSearch: (p: TPet[] | undefined, s: string) => TPet[];
+  filterSearch: (p: TPet[] | undefined | null, s: string) => TPet[]|null|undefined;
 };
 export const useSearchStore = create<TSearchStore>((set, get) => ({
   search: "",
@@ -12,12 +12,16 @@ export const useSearchStore = create<TSearchStore>((set, get) => ({
     set(() => ({ search: s }));
   },
   filterSearch: (pets, s) => {
-    if (pets) {
+    //if pets is null, the id is still unset and the pets are being queried from db
+    if(pets === null) return null;
+    //if pets is undefined, the id was set but query threw an error
+    if(pets === undefined) return undefined;
+    //else there is an array of pets
+    else {
       const filtered = pets.filter((p) =>
         p.name.toLocaleLowerCase().includes(s.toLowerCase())
       );
       return filtered;
     }
-    return [];
   },
 }));
