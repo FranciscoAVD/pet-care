f75d4a07668cc7e2fac2b937f868d6bfa9b08519
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-07 16:19:37 -0400

    app/app/layout is no longer a client component and the storeUser hook has been moved to the dashboard page. account page has new background from public/bulldog.jpg. Dashboard page is now client component and handles all the querying of pets from db and storing user on page load.

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,12 +1,35 @@
+"use client";
+import bulldog from "../../../../../public/bulldog.jpg";
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
-import { SignOutButton } from "@clerk/nextjs";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
 export default function Account() {
+  const { user } = useUser();
   return (
     <main>
-      <H1 className="my-8">Your Account</H1>
-      <SignOutButton/>
-      <ContentBlock className="h-[500px] flex items-center justify-center">Logged in as ...</ContentBlock>
+      <div className="flex justify-between items-center">
+        <H1 className="my-8">Your Account</H1>
+        <div className="flex gap-x-2 items-center">
+          <UserButton />
+        </div>
+      </div>
+
+      <ContentBlock className="relative h-[500px] flex items-center justify-center bg-transparent">
+        <div className="absolute h-full w-full image-gradient">
+          <Image
+            src={bulldog}
+            alt="bulldog smiling in the background"
+            className="absolute object-cover sm:object-contain w-auto h-full left-0 top-0 -z-[1]"
+          />
+        </div>
+        <div className="relative z-10">
+        Logged in as{" "}
+        <span className="uppercase font-semibold block">{user?.username}</span>
+        </div>
+        
+      </ContentBlock>
     </main>
   );
 }
--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import AddPetButton from "@/components/add-pet-button";
 import Branding from "@/components/branding";
 import ContentBlock from "@/components/content-block";
@@ -5,33 +6,37 @@ import PetDetails from "@/components/pet-details";
 import PetList from "@/components/pet-list";
 import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
-import { TPet } from "@/lib/types";
+import useStoreUserEffect from "@/hooks/useStoreUser";
+import { useQuery } from "convex/react";
+import { api } from "../../../../../convex/_generated/api";
+import { usePetStore } from "@/stores/pet-store";
+import { useSearchStore } from "@/stores/search-store";
 
-
-export default async function Dashboard() {
-  let pets: TPet[] = [];
-  const res = await fetch(
-    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
-  );
-  if (!res.ok) throw new Error("Could not fetch pets");
-  else pets = await res.json();
-
+export default function Dashboard() {
+  const id = useStoreUserEffect();
+  const pets = useQuery(api.pets.getPets, { user: id });
+  const getPet = usePetStore((state) => state.getActivePet);
+  const active = usePetStore((state) => state.activePet);
+  const pet = getPet(pets, active);
+  const filtered = useSearchStore((state) => state.filterSearch);
+  const search = useSearchStore((state) => state.search);
+  
   return (
     <main>
       <div className="flex justify-between sm:items-center py-8">
         <Branding />
-        <Stats guests={pets.length} />
+        <Stats guests={pets?.length}/>
       </div>
       <div className="grid md:grid-cols-3 grid-rows-[45px_300px_500px] md:grid-rows-[45px_1fr] md:h-[600px] gap-4">
         <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
           <SearchForm />
         </div>
         <ContentBlock className="relative md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
-          <PetList pets={pets} />
-          <AddPetButton className="absolute right-4 bottom-4 z-10"/>
+          <PetList pets={filtered(pets, search)}/>
+          <AddPetButton className="absolute right-4 bottom-4 z-10" id={id}/>
         </ContentBlock>
         <ContentBlock className="md:row-start-1 md:row-span-full md:col-start-2 md:col-span-full">
-          <PetDetails pets={pets} />
+          <PetDetails pet={pet}/>
         </ContentBlock>
       </div>
     </main>
