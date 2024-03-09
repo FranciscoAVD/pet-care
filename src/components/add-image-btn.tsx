e55159167526c6e10b9c7ccb4613abdca5ab613f
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-09 13:12:42 -0400

    finished ui for button now found in pet details and shown only if there is no imageUrl defined in the pet

--- src/components/add-image-btn.tsx
+++ src/components/add-image-btn.tsx
@@ -3,20 +3,27 @@ import { UploadButton, UploadFileResponse } from "@xixixao/uploadstuff/react";
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { useState } from "react";
-export default function ImageButton({  }) {
+import "@xixixao/uploadstuff/react/styles.css";
+import { ImageIcon } from "@radix-ui/react-icons";
+import { usePetStore } from "../stores/pet-store";
+export default function ImageButton({ children }:{children?: React.ReactNode}) {
   const [uploadId, setUploadId] = useState<null | string>(null);
   const generateUploadUrl = useMutation(api.files.generateUploadUrl);
-  const saveAfterUpload = (uploaded: UploadFileResponse[]) => {
-    setUploadId((uploaded[0].response as any).storageId);
+  const patchPetImage = useMutation(api.pets.addImage)
+  const activePet = usePetStore(state=> state.activePet);
+  const saveAfterUpload = async (uploaded: UploadFileResponse[]) => {
+    const res = await (uploaded[0].response as any).storageId
+    //@ts-ignore
+    patchPetImage({pet: activePet, imageId: res})
   };
   return (
-    <>
       <UploadButton
+        content={()=>"Add Image"}
+        className={()=>"text-sm hover:cursor-pointer font-semibold"}
         uploadUrl={generateUploadUrl}
         fileTypes={["image/*"]}
         onUploadComplete={saveAfterUpload}
         onUploadError={(error: unknown) => alert(`ERROR: ${error}`)}
       />
-    </>
   );
 }
