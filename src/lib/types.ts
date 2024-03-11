dba976d9a798994b0e14ef5df9a36e247ad1ffc7
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-03-11 15:10:14 -0400

    added possible type to TPet according to the convex pet return type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,24 @@
 import { Id } from "../../convex/_generated/dataModel";
 
 export type TPet = {
-    _id: Id<"pets">;
-    _creationTime: number;
-    notes?: string | undefined;
-    imageStorageId?: Id<"_storage"> | undefined;
-    imageUrl?: string | undefined;
-    careTaker: Id<"users">;
-    name: string;
-    age: number;
-    owner: string;
+  _id: Id<"pets">;
+  _creationTime: number;
+  notes?: string | undefined;
+  imageStorageId?: Id<"_storage"> | undefined;
+  imageUrl?: string | null | undefined;
+  careTaker: Id<"users">;
+  name: string;
+  age: number;
+  owner: string;
 };
+
+/*{ _id: Id<"pets">; 
+_creationTime: number; 
+notes?: string | undefined;
+ imageStorageId?: Id<"_storage"> | undefined;
+  imageUrl?: string | null | undefined;
+   careTaker: Id<"users">;
+    name: string;
+     age: number;
+      owner: string;
+     }[] | null | undefined */
